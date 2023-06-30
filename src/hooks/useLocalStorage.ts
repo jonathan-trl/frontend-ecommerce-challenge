3c9f24730febd0212a87964b73d817881912c104
Jonathan Carvalho <jonathan.carvalho1807@gmail.com>
2023-06-30 17:51:44 -0300

    Correção local storage server side

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -3,19 +3,24 @@ import { useEffect, useState } from 'react'
 
 function useLocalStorage<T>(key: string, initialValue: T) {
   const { setCartLength } = useCountCartLength()
-  const storedValue = localStorage.getItem(key)
-  const parsedValue = storedValue ? JSON.parse(storedValue) : initialValue
+
+  let parsedValue = initialValue
+
+  if (typeof window !== 'undefined') {
+    const storedValue = localStorage.getItem(key)
+    parsedValue = storedValue ? JSON.parse(storedValue) : initialValue
+  }
 
   const [value, setValue] = useState<T>(parsedValue)
 
   const updateLocalStorage = (newValue: T) => {
     setValue(newValue)
-    localStorage.setItem(key, JSON.stringify(newValue))
+    if (typeof window !== 'undefined') {
+      localStorage.setItem(key, JSON.stringify(newValue))
+    }
   }
 
   useEffect(() => {
-    console.log('caiu aqui')
-
     setCartLength((value as T[]).length)
   }, [value])
 
