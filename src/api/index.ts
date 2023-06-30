7127fe5dbd0cd1310298dab42d0a39a483f35d86
Jonathan Carvalho <jonathan.carvalho1807@gmail.com>
2023-06-30 18:19:21 -0300

    Corrigido url api para .env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:3333/' })
+const url = process.env.NEXT_PUBLIC_API_URL
+
+const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL })
 
 export default api
